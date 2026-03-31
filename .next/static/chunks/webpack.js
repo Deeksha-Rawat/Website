7bbee8b12baff77b8ec8d05dec6209995764c7f2
Deeksha-Rawat <deeksha9557@gmail.com>
2026-03-31 11:07:54 +0530

    Add netlify.toml with Node 18 and build config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19f055e1fb28959a"; }
+/******/ 		__webpack_require__.h = function() { return "e679bac355bd3814"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
